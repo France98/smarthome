1fa3c72035d502b03f7032cbbd8db73a5bfc23ad
France98 <larn_francisza@hotmail.com>
2017-07-23 16:01:19 +0700

    dd

--- house.js
+++ house.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    var link = "http://158.108.165.223/data/CEEDney"
+    var link = "http://158.108.165.223/data/CEEDney/"
 
     $('#light').click(function () {
-        var open = "1";
+        var open = "1-0-0-0-0-0";
         $.ajax({
             url: link + "set/" + open
         }).done(function () {
@@ -14,7 +14,7 @@ $(document).ready(function () {
     });
 
     $('#air').click(function () {
-        var open = "01";
+        var open = "0-1-0-0-0-0";
         $.ajax({
             url: link + "set/" + open
         }).done(function () {
@@ -26,10 +26,10 @@ $(document).ready(function () {
     });
 
     $('#door').click(function () {
-        var open = "001"
+        var open = "0-0-1-0-0-0"
         $.ajax({
             url: link + "set/" + open
-        }).done(function () {      
+        }).done(function () {
             console.log(open)
             console.log("Done");
         }).fail(function () {
@@ -37,19 +37,35 @@ $(document).ready(function () {
         });
     });
 
-    setInterval(function(){
+    setInterval(function () {
         $.ajax({
             url: link
-        }).done(function(data){
+        }).done(function (data) {
             var code = data.split("-")
+            console.log($('#num').text(code[3]));
+            console.log($('#temp').text(code[4]));
+            console.log($('#inten').text(code[5]));
+            if (code[0] == 1) {
+                console.log($('#light2').text("on"));
+            } else {
+                console.log($('#light2').text("off"));
+            }
+            if (code[1] == 1) {
+                console.log($('#air2').text("on"));
+            } else {
+                console.log($('#air2').text("off"));
+            }
+            if (code[2] == 1) {
+                console.log($('#door2').text("open"));
+            } else {
+                console.log($('#door2').text("close"));
+            }
 
-            console.log($('#num').text(code[3]))
-            console.log($('#temp').text(code[4]))
-            console.log($('#inten').text(code[5]))
+
             $('#temp').val(data);
-        }).fail(function(data){
+        }).fail(function (data) {
             console.error("failed");
         });
-    }, 1000*2);
+    }, 1000 * 2);
 
 });
