a4dc6e3ae033714086d9bd5b881dd2771f0c3032
France98 <larn_francisza@hotmail.com>
2017-07-24 14:07:46 +0700

    add

---
+++ Exceed-master/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#def').click(function () {
+        $('#p').fadeOut("slow");
+        
+
+    });
+});
--- pro.js
+++ pro.js
@@ -1,12 +1,56 @@
 $(document).ready(function(){
     var link = "http://158.108.165.223/data/CEEDney/"
 
+    $('#fan').click(function () {
+        var open = "";
+        $.ajax({
+            url: link + "set/" + open
+        }).done(function () {
+            console.log(open)
+            console.log("Done");
+        }).fail(function () {
+            console.log("Failed");
+        });
+    });
+
+    $('#lang').click(function () {
+        var open = "";
+        $.ajax({
+            url: link + "set/" + open
+        }).done(function () {
+            console.log(open)
+            console.log("Done");
+        }).fail(function () {
+            console.log("Failed");
+        });
+    });
+
+    $('#water').click(function () {
+        var open = "";
+        $.ajax({
+            url: link + "set/" + open
+        }).done(function () {
+            console.log(open)
+            console.log("Done");
+        }).fail(function () {
+            console.log("Failed");
+        });
+    });
+
     setInterval(function () {
         $.ajax({
             url: link
         }).done(function (data) {
             var code = data.split("-")
             console.log($('#tem').text(code[0]));
+            console.log($('#light').text(code[1]));
+            console.log($('#tank').text(code[2]));
+            console.log($('#moist').text(code[3]));
+            if(code[4] === 1){
+                console.log($('#servo').text("Open"));
+            }else{
+                console.log($('#servo').text("Close"));
+            }
         }).fail(function (data) {
             console.error("failed");
         });
