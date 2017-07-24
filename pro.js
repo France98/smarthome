b8dcb135bad13f4b7ba15e80a255d4495e144539
France98 <larn_francisza@hotmail.com>
2017-07-24 17:33:02 +0700

    add

--- pro.js
+++ pro.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var link = "http://158.108.165.223/data/CEEDney/"
+    
+
 
     $('#fan').click(function () {
         var open = "";
@@ -37,6 +39,18 @@ $(document).ready(function(){
         });
     });
 
+    $('#water').click(function () {
+        var open = "1";
+        $.ajax({
+            url: link + "set/" + open
+        }).done(function () {
+            console.log(open)
+            console.log("Done");
+        }).fail(function () {
+            console.log("Failed");
+        });
+    });
+
     setInterval(function () {
         $.ajax({
             url: link
@@ -44,12 +58,27 @@ $(document).ready(function(){
             var code = data.split("-")
             console.log($('#tem').text(code[0]));
             console.log($('#light').text(code[1]));
-            console.log($('#tank').text(code[2]));
             console.log($('#moist').text(code[3]));
-            if(code[4] === 1){
+            var distance = code[2] / 29.1 /2; //centimeter
+            var volume = (Math.PI * Math.pow(3,2)) * 22.5
+            if (code[4] === 1) {
                 console.log($('#servo').text("Open"));
-            }else{
+            } else {
                 console.log($('#servo').text("Close"));
+            }
+            if(code[3]>=25 && code[3]<=34){
+                console.log($('#talk').text("สบายจังเลย"));
+            }else{
+                console.log($('#talk').text("ไม่สบายแล้วนะ"));
+            }
+            if (distance === 0 ) {
+                console.log($('#tank').text("Full"));
+            }else if(distance >= 15){
+                console.log($('#talk').text("น้ำน้อยแล้วนะจ้ะ"));
+                console.log($('#tank').text(volume));
+            }else{
+                console.log($('#talk').text("ยังมีน้ำในถังนะ"));
+                console.log($('#tank').text(volume));
             }
         }).fail(function (data) {
             console.error("failed");
